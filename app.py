0d81aa51d1f62c8b75ed2913d358e2cee9d390fb
daniel-wesh <daniel.waweru@outlook.com>
2023-03-01 15:26:45 +0300

    Added html template files to the project + added a route on app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from flask import flask, request
+from flask import Flask, request, render_template, redirect
 
-app = 
+app = Flask(__name__)
+
+REGISTRANTS = {}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods = ["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    REGISTRANTS[name] = sport
+    return redirect("./registrants")
+
+@app.route("./registrants")
+def registrants():
+    return render_template("registrants.html")
